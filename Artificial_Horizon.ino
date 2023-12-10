c23b9c96561cf4bbeb079eb909c1547966a16ee9
Farid <101432221+fvreeed@users.noreply.github.com>
2023-12-10 23:55:19 +0300

    Update Artificial_Horizon.ino

--- Artificial_Horizon.ino
+++ Artificial_Horizon.ino
@@ -1,30 +1,130 @@
+#include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+#include "Kalman.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 64
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 
-#define OLED_ADDR   0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
+MPU6050 gyro;
+
+int16_t AccX, AccY, AccZ, GyroX, GyroY, GyroZ;
+
+double accelScale = 2.0 / 32768.0;
+double gyroScale = 250.0 / 32768.0;
+
+uint8_t fifoBuffer[45];
+
+uint32_t timer;
 
 extern const unsigned char logo[];
 
 void setup() {
-  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
-  
-  introduction();
+
+  Serial.begin(9600);
+
+  startOLED();
+  introductionOLED();
+
+  gyro.initialize();
+  gyro.dmpInitialize();
+  gyro.setDMPEnabled(true);
+
+  gyro.CalibrateGyro(10);
+  gyro.CalibrateAccel(10);
 }
 
 void loop() {
-  display.clearDisplay();
-  display.drawLine(48, 32, 80, 32, WHITE);
 
-  display.display();
+  if (millis() - timer >= 11) {  // таймер на 11 мс (на всякий случай)
+    if (gyro.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+
+      Quaternion q;
+      VectorFloat gravity;
+      float zyx[3];
+
+      gyro.dmpGetQuaternion(&q, fifoBuffer);
+      gyro.dmpGetGravity(&gravity, &q);
+      gyro.dmpGetYawPitchRoll(zyx, &q, &gravity);
+
+      Serial.print(zyx[0] * 180 / M_PI); // вокруг оси Z
+      Serial.print(',');
+      Serial.print(zyx[1] * 180 / M_PI); // вокруг оси Y
+      Serial.print(',');
+      Serial.print(zyx[2] * 180 / M_PI); // вокруг оси X
+      Serial.println();
+
+      display.clearDisplay();
+
+      aimOLED();
+
+      double pitch = getPitch(zyx[1]);
+
+      double roll = getRoll(zyx[0]);
+
+      int16_t yaw = getYaw(zyx[2]);
+      
+      compassOLED(yaw);
+
+      display.drawLine(0, 32 + roll / 1.40625 + pitch / 1.8, 128, 32 - roll / 1.40625 + pitch / 1.8, WHITE);
+
+      display.display();
+
+      delay(100);
+  
+      timer = millis();
+    }
+  }
 }
 
-void introduction() {
+double getPitch(float y) {
+  // Угол тангажа
+  // pitch = atan2(acc_x, sqrt(acc_y^2 + acc_z^2))
+
+  return y * 180 / M_PI;
+}
+
+double getRoll(float x) {
+  // Угол крена
+  // roll = atan2(-acc_y, acc_z)
+
+  return x * 180 / M_PI;
+}
+
+double  getYaw(float z) {
+  // Угол рыскания
+  // yaw = gyro_z*dt
+
+  return z * 180 / M_PI;
+}
+
+void compassOLED(int16_t yaw) {
+  yaw = yaw * -1;
+  if (yaw < 0) {
+    yaw = 360 + yaw;
+  }
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(57, 6);
+  display.print(yaw);
+}
+
+void startOLED() {
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+}
+
+void introductionOLED() {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -51,4 +151,13 @@ void introduction() {
   display.display();
 
   delay(3000);
+}
+
+void aimOLED() {
+  display.clearDisplay();
+  display.drawLine(40, 32, 56, 32, WHITE);
+  display.drawLine(72, 32, 88, 32, WHITE);
+  display.drawLine(56, 32, 56, 36, WHITE);
+  display.drawLine(72, 32, 72, 36, WHITE);
+  display.drawLine(56, 36, 72, 36, WHITE);
 }
